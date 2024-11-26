935ad6d2a0cf4b48a829c6b281358b805eeda4e9
Kondrateva Ekaterina <kondrateva-ekaterina@ro.ru>
2024-11-26 15:55:27 +0300

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,34 +1,32 @@
 def area(a, b):
     """
-    Вычисляет площадь прямоугольника по его ширине и высоте.
-
-    Параметры:
-    a (float): Ширина прямоугольника.
-    b (float): Высота прямоугольника.
-
-    Возвращает:
-    float: Площадь прямоугольника.
-
-    Пример вызова:
-    >>> area(4, 5)
-    20
+    This function eval area of rectangle
+        Parameters:
+            a: side length of rectangle
+            b: side length of rectangle
+        Return:
+            area: area of rectangle
+        Example:
+            area(2, 4): 8
     """
-    return a * b
+    if str(a).isdigit() and str(b).isdigit() and int(a) > 0 and int(b) > 0:
+        return int(a) * int(b)
+    else:
+        return 0
 
 
 def perimeter(a, b):
     """
-    Вычисляет периметр прямоугольника по его ширине и высоте.
-
-    Параметры:
-    a (float): Ширина прямоугольника.
-    b (float): Высота прямоугольника.
-
-    Возвращает:
-    float: Периметр прямоугольника.
-
-    Пример вызова:
-    >>> perimeter(4, 5)
-    18
+    This function eval perimeter of rectangle
+        Parameters:
+            a: side length of rectangle
+            b: side length of rectangle
+        Return:
+            perimeter: perimeter of rectangle
+        Example:
+            perimeter(2, 4): 12
     """
-    return (a + b) * 2
+    if str(a).isdigit() and str(b).isdigit() and int(a) > 0 and int(b) > 0:
+        return (int(a) + int(b)) * 2
+    else:
+        return 0
